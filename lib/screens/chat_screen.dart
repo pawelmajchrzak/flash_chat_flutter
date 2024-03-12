ccb1863f9d740779f96899cbd1831382d172aae3
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2024-03-12 23:27:09 +0100

    finishd App

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 final _firestore = FirebaseFirestore.instance;
+late User loggedInUser;
 
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
@@ -14,7 +15,6 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  late User loggedInUser;
   late String messageText;
 
   @override
@@ -122,17 +122,21 @@ class MessagesStream extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data?.docs;
+        final messages = snapshot.data?.docs.reversed;
         List<MessageBubble> messageBubbles = [];
         for (var message in messages!) {
           var data = message.data() as Map;
           final messageText = data['text'];
           final messageSender = data['sender'];
-          final messageBubble = MessageBubble(messageSender, messageText);
+
+          final currentUser = loggedInUser.email;
+
+          final messageBubble = MessageBubble(messageSender, messageText, currentUser == messageSender);
           messageBubbles.add(messageBubble);
         }
         return Expanded(
           child: ListView(
+            reverse: true,
             shrinkWrap: true,
             padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
             children: messageBubbles,
@@ -144,34 +148,36 @@ class MessagesStream extends StatelessWidget {
 }
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble(this.sender, this.text);
+  MessageBubble(this.sender, this.text, this.isMe);
   final String sender;
   final String text;
+  final bool isMe;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
           Text(
             sender,
-            style: TextStyle(
-              fontSize: 12.0,
-              color: Colors.black54
-            ),
+            style: TextStyle(fontSize: 12.0, color: Colors.black54),
           ),
           Material(
-            borderRadius:  BorderRadius.circular(30.0),
+            borderRadius: BorderRadius.only(
+                topLeft: isMe ? Radius.circular(30.0) : Radius.circular(0),
+                topRight: isMe ? Radius.circular(0) : Radius.circular(30.0),
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0)),
             elevation: 5.0,
-            color: Colors.lightBlueAccent,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
               child: Text(
                 text,
                 style: TextStyle(
-                  color: Colors.white,
+                  color: isMe ? Colors.white : Colors.black54,
                   fontSize: 15.0,
                 ),
               ),
